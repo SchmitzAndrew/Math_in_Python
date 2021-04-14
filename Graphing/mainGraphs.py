4c6387e53ef56b44ef3b6b2084572efe7f4d0c77
A_SCHMITZ <schmitzandrew03@gmail.com>
2021-04-13 23:14:09 -0700

    4D graph

--- Graphing/mainGraphs.py
+++ Graphing/mainGraphs.py
@@ -10,13 +10,17 @@ print("Graphing with matplotlib")
 print("2D Graphs: ")
 print("1 - Modulus")
 print("2 - Polynomial")
+
+print("Multi Dimensional Graphs: ")
+
 print("3 - 3D Scattered Graph")
 print("4 - Contour")
 print("5 - Wireframe")
 print("6 - Surface Plot ")
 print("7 - Surface Triangulations")
 print("8 - Mobius Strip")
-print("3D Graphs: ")
+print("9 - 4D Graph")
+
 
 
 selection = int(input("Enter the method to call: "))
@@ -42,6 +46,8 @@ elif selection == 7:
     graph3D.surface_triangulations()
 elif selection == 8:
     graph3D.mobius()
+elif selection == 9:
+    multiDScatterPlot()
 else:
     print("Not a valid selection")
 
--- Graphing/multiDimensionalGraphs.py
+++ Graphing/multiDimensionalGraphs.py
@@ -1,7 +1,18 @@
 import pandas as pd
 import plotly
+import plotly.graph_objs as go
+
 
 
 def multiDScatterPlot():
-    data = pd.read_csv("student-data.csv")
+    col_list = ["age", "freetime", "goout", "studytime"]
+    data = pd.read_csv("student-mat.csv",  usecols=col_list)
+
+    markercolor = data['age']
+
+    figure = go.Scatter3d(x=data['freetime'], y=data['goout'],  z=data['studytime'], marker=dict(color=markercolor,opacity=1,reversescale=True,colorscale='Blues',size=5),line=dict (width=0.02), mode='markers')
+
+    layout = go.Layout(scene=dict(xaxis =dict( title="Free Time"), yaxis=dict( title="Go Out"), zaxis=dict( title="Study Time")),)
+
+    plotly.offline.plot({"data": [figure], "layout": layout}, auto_open=True, filename=("4DPlot.html"))
 
